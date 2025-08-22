1cd1872dfd2d24030e1f12ab0601146f229952fe
olayinka <adeleyeolayinka73@gmail.com>
2025-08-22 10:27:20 +0100

    add home navbar and resumecards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "TalentScreen-AI" },
+    { name: "description", content: "AI-powered resume analyzer by Adeleye Olayinka Ibrohim that scans, scores, and optimizes your CV. Get instant insights, keyword checks, and tailored tips to land your dream job.”!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.png')] bg-cover">
+
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Manage Applications and Resume Ratings</h1>
+        <h2>Monitor submissions and optimize resumes with AI.</h2>
+      </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+
+    </section>
+  </main>
 }
