d2276e22b10be1ce2b9fd173345e7a80617072c1
olayinka <adeleyeolayinka73@gmail.com>
2025-08-22 22:04:31 +0100

    setup puter and create the auth page and functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,10 @@ import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
 
+import { useEffect } from 'react'
+import { useNavigate } from "react-router";
+import { usePuterStore } from '~/lib/puter';
+
 export function meta({ }: Route.MetaArgs) {
   return [
     { title: "TalentScreen-AI" },
@@ -11,6 +15,16 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+  const { isLoading, auth } = usePuterStore();
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+
   return <main className="bg-[url('/images/bg-main.png')] bg-cover">
 
     <Navbar />
